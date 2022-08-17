2aa257ef25251be89286e19023593c9e96733aac
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-08-17 19:17:29 +0200

    overkill?

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@ import 'quartermoon/css/quartermoon-variables.css'
 import Loader from './modules/Loader.svelte'
 </script>
 
-<div class='page-wrapper'>
+<div class='page-wrapper bg-dark'>
   <!-- Transitions -->
   <Loader />
   <div class='content-wrapper overflow-x-hidden'>
--- src/modules/Loader.svelte
+++ src/modules/Loader.svelte
@@ -1,16 +1,32 @@
 <script>
-let transition = false
-let hide = false
+import { onMount } from 'svelte'
 
-setTimeout(() => {
-  hide = true
-  transition = true
-}, 2000)
+let hide = false
+let svg = false
+
+function hideLoader () {
+  setTimeout(() => {
+    svg = false
+    hide = true
+  }, 2000)
+}
+onMount(() => {
+  setTimeout(() => {
+    svg = true
+  }, 1000)
+})
 </script>
 
+<svelte:window on:DOMContentLoaded={hideLoader} />
+
 <div class='w-full h-full con font-weight-bold z-100 overflow-hidden' class:hide>
-  <div class='h-full text d-flex align-items-center justify-content-center anim' class:transition>
-    Epilepsy Warning
+  <div class='h-full text d-flex align-items-center justify-content-center text-nowrap' class:transition={hide}>
+    <div class={svg ? 'd-none' : 'd-flex'}>Epilepsy Warning</div>
+    <svg class='w-full h-full {svg ? 'd-flex' : 'd-none'}'>
+      <text x='50%' y='50%' dy='4.15rem' text-anchor='middle' font-size='10rem' font-weight='700' stroke-width='1px'>
+        Epilepsy Warning
+      </text>
+    </svg>
   </div>
 </div>
 
@@ -23,18 +39,67 @@ setTimeout(() => {
     from { background-position: top left }
     to { background-position: top right }
   }
+  @keyframes stroke {
+    0%   {
+      stroke: #fff;
+      fill: #000;
+    }
+    20%   {
+      fill: #000;
+      background: #000;
+    }
+    21%  {
+      background: #fff;
+      fill: #000;
+    }
+    40%  {
+      fill: #000;
+      stroke: #fff;
+    }
+    41%  {
+      fill: #fff;
+      stroke: #000;
+    }
+    60%  {
+      background: #fff;
+      fill: #fff;
+      stroke: #000;
+    }
+    61%  {
+      background: none;
+      fill: #000;
+      stroke: #fff;
+    }
+    80%  {
+      fill: #000;
+      stroke: #fff;
+    }
+    81%  {
+      fill: #fff;
+      stroke: #000;
+    }
+    100% {
+      background: none;
+      fill: #fff;
+      stroke: #000;
+    }
+  }
+  svg {
+    animation: stroke .8s forwards;
+    fill: #000;
+    stroke: #fff;
+  }
   .con {
-    transition: 1s ease;
+    transition: width 1s ease;
+  }
+  svg {
+    font-size: 10rem;
   }
   .text {
-    width: 100vw;
-    text-align: center;
-    white-space: nowrap;
     font-size: 10rem;
     color: transparent;
-    background:
-      linear-gradient( 90deg, #fff 50%, #000 0),
-      linear-gradient( 90deg, #000  50%, #fff 0);
+    width: 100vw;
+    background: linear-gradient(90deg, #fff 50%, #000 0), linear-gradient(90deg, #000 50%, #fff 0);
     background-repeat: no-repeat;
     background-size: 200% 100%;
     background-position: top right;
@@ -42,7 +107,7 @@ setTimeout(() => {
     background-clip: text, padding-box;
     animation: .8s background ease forwards;
   }
-  .text.transition {
+  .transition {
     animation: .8s un-background ease forwards;
   }
   .hide {
